8cd616891043f9441a141070b9e24b7c55c7b525
Jakub Karcz <92019651+FazKarcz@users.noreply.github.com>
2022-02-23 15:12:30 +0100

    Update nowy.py

--- nowy.py
+++ nowy.py
@@ -1,4 +1,5 @@
 import sys
+import math
 print(sys.version)
 
 
@@ -6,3 +7,15 @@ x = 5.2348289462
 print(int(3))
 print(x)
 print("sussy b-baka")
+
+c=2+2j
+print(c)
+
+g= 2.565463434
+k= 2.526213
+print(g+k)
+
+a=5
+b=3
+c= a - b
+print("wynik dzialania {0:d} - {1:d} = {2:c}".format(a,b,c))
