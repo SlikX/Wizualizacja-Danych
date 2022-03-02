b915203b14c334f99d9ce60ee7707cf2c4719b45
Jakub Karcz <92019651+FazKarcz@users.noreply.github.com>
2022-03-02 15:26:06 +0100

    Create Lekcja_2.py

---
+++ Lekcja_2.py
@@ -0,0 +1,78 @@
+ #a = input('Jaka pierwsza liczba: ')
+ #b = input('Jaka druga liczba: ')
+ #c = input('Jaka trzecia liczba: ')
+ #d = input('Jaka czwarta liczba: ')
+ #a=int(a)
+ #b=int(b)
+ #c=int(c)
+ #d=int(d)
+ #if (a > b) and (c>b):
+ #    print('liczba A oraz C wieksza od liczby B')
+#else:
+#     print('Liczba B jest więkasz od A i C')
+
+#for cos in range(0,7,1):
+# print(cos)
+
+#lista = ['b',1,6.9,8,'cos']
+#for cos in lista:
+#    print(cos)
+#else:
+#    print('Wszystkie elementy listy')
+#a = 0
+#while a < 11:
+#   print(a)
+#    a += 1
+
+
+
+#lista = [6,9,6,9,6,9]
+#a = input('Podaj liczbe: ')
+#licznik = 0
+#while licznik < len(lista):
+#    if int(a) - lista[licznik] == 0:
+#        print('liczba ' + str(a) + ' - ' + str(lista[licznik]) + ' = 0')
+#        break
+#    else:
+#        licznik +=1
+#else:
+#    print('Żadna z wartości nie dała odpowiedniego wyniku')
+
+#tab1= [1,2,3,4,5,6]
+#ab2= [4,5,1,6]
+#uma=[]
+#for a in tab1:
+#    for b in tab2:
+#        wynik = a+b
+#        suma.append(wynik)
+#    print(suma)
+
+#a= input('Pierwsza liczba: ')
+#b= input('Druga liczba: ')
+#try:
+#    a=int(a)
+#    b=int(b)
+#    wynik = a/b
+#    print(wynik)
+#except ZeroDivisionError:
+#    print('Pamiętaj cholero żeby nie dzielić przez zero')
+#except ValueError:
+#    print('Co ty robisz człowieku?? Daj INT!')
+
+#lista= [7,2,9,3,6,2,2]
+#slownik={1:20,2:20}
+#lista.sort()
+#print(lista)
+#print(lista.pop(4))
+#print(lista.count(2))
+#lista.insert(2,4)
+#print(lista)
+#lista.remove(2)
+#print(lista)
+#print(slownik.pop(2))
+#print(slownik.keys())
+#print(slownik.popitem())
+#lista.reverse()
+#print(lista)
+#print(lista.clear())
+#print(slownik.clear())
